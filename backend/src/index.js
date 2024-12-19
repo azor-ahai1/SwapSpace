1b2fd13b5c6be9d4ff26b5f4bb1e2d0af575c625
Aashish Shukla <aks1092002@gmail.com>
2024-12-20 03:48:27 +0530

    DeployTry5

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
 // --------------------------------
+import * as dotenv from "dotenv"
 import path from "path"
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({
-    path: path.resolve(process.cwd(), '.env')
+    path: path.resolve(__dirname, '../.env')
 })
 
 // -------------------------------
