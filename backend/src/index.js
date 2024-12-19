bb0aa768a4117e7b9532146d57f40a8366ebeb80
Aashish Shukla <aks1092002@gmail.com>
2024-12-19 23:46:34 +0530

    DeployTry2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,21 @@
 import dotenv from "dotenv"
+// --------------------------------
+import path from "path"
+
+dotenv.config({
+    path: path.resolve(process.cwd(), '.env')
+})
+
+// -------------------------------
+
+
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-dotenv.config({
-    path: "./.env"
-})
+
+// dotenv.config({
+//     path: "./.env"
+// })
 
 connectDB().then(
     () => {
