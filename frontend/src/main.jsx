9a33b82650f879620a8d6c1e540ea85370ca7df2
Aashish Shukla <aks1092002@gmail.com>
2024-12-23 21:41:40 +0530

    DeployTry22

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store, { persistor } from './store/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, HashRouter} from 'react-router-dom'
 // import {Protected} from './components/index.js'
 
 import {Home, Login, Signup, AddProduct, ViewProduct, AllProducts, UserProfile, EditProfile, EditProduct} from './pages/index.js'
@@ -59,7 +59,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={false} persistor={persistor}>
-        <RouterProvider router={router} />
+        <HashRouter>
+          <RouterProvider router={router} />
+        </HashRouter>
       </PersistGate>
     </Provider>
   </StrictMode>,
