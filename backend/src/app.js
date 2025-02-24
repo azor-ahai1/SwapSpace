8ebbfe1c01af03c24fc7f4c7da3b526dcb590762
Aashish Shukla <aks1092002@gmail.com>
2025-02-24 16:40:29 +0530

    DeployTry37

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
